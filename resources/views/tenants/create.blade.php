0a0dfc8e4615f5aef01f385899dce609fe8a690f
Vanessa Klotzman <vanessa.klotzman@metalab.csun.edu>
2017-11-13 19:17:33 -0800

    almost done

---
+++ resources/views/tenants/create.blade.php
@@ -0,0 +1,167 @@
+<html>
+<head>
+   
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+   <title> Hello there</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- References: https://github.com/fancyapps/fancyBox -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+ 
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+
+
+      
+
+   
+   
+
+    <style type="text/css">
+    .gallery
+    {
+        display: inline-block;
+        margin-top: 20px;
+    }
+    .close-icon{
+        border-radius: 50%;
+        position: absolute;
+        right: 5px;
+        top: -10px;
+        padding: 5px 8px;
+    }
+    .form-image-upload{
+        background: #e8e8e8 none repeat scroll 0 0;
+        padding: 15px;
+    }
+    </style>
+</head>
+<body>
+
+    
+    @if (Route::has('login'))
+                <div class="top-right links">
+
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/tenants') }}">Tenants</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                       
+                    @endauth
+                </div>
+            @endif
+
+
+@auth
+
+
+<h1>Create a Tenant</h1>
+
+<!-- if there are creation errors, they will show here -->
+
+
+{{ Form::open(array('url' => 'tenants')) }}
+
+    <div class="form-group">
+        {{ Form::label('tenant_name', 'Name') }}
+        {{ Form::text('tenant_name', Input::old('tenant_name'), array('class' => 'form-control')) }}
+    </div>
+     <div class="form-group">
+        {{ Form::label('tenant_lastname', 'Last Name') }}
+        {{ Form::text('tenant_lastname', Input::old('name'), array('class' => 'form-control')) }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('email', 'Email') }}
+        {{ Form::email('email', Input::old('email'), array('class' => 'form-control')) }}
+    </div>
+
+     <div class="form-group">
+        {{ Form::label('phone_number', 'Phone Number') }}
+        {{ Form::text('phone_number', Input::old('Phone Number'), array('class' => 'form-control')) }}
+    </div>
+
+     <div class="form-group">
+        {{ Form::label('currentlyat', 'Currently At') }}
+        {{ Form::text('currentlyat', Input::old('Currently At'), array('class' => 'form-control')) }}
+    </div>
+
+
+
+  
+    {{ Form::submit('Create A Tenant!', array('class' => 'btn btn-primary')) }}
+
+{{ Form::close() }}
+
+@else
+
+<h1> You are not authorized to view this page</h1>
+
+@endauth
+</div>
+</body>
+</html>
