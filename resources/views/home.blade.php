45513a34355ba5058882c6eb085c308c51555c35
Vanessa Klotzman <vanessa.klotzman@metalab.csun.edu>
2017-11-24 19:53:54 -0800

    accounts part is gucci

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -77,7 +77,7 @@
                     <img src="https://i.imgur.com/JUPgzwZ.png" class="img-fluid" alt="Card image cap" id="cardImg">
                     <div class="card-body">
                         <p class="card-text">
-                          <a href="{{ url('/tenants')}}" class="btn btn-secondary btn-lg btn-block" role="button"> Add, Delete, or Edit Accounts Receivable <i class="fa fa-user-circle-o" aria-hidden="true"></i></i> </a>
+                          <a href="{{ url('/accounts')}}" class="btn btn-secondary btn-lg btn-block" role="button"> Add, Delete, or Edit Accounts Receivable <i class="fa fa-user-circle-o" aria-hidden="true"></i></i> </a>
                         </p>
                     </div>
                 </div>
