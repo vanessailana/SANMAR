97eb92313a008560608d37d8c66aa10a84527d21
Vanessa Klotzman <vanessa.klotzman.17@my.csun.edu>
2017-12-05 15:33:08 -0800

    fixed migrations

--- app/ContactUS.php
+++ app/ContactUS.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class ContactUS extends Model
 {
  
-public $table = 'contactus';
+public $table = 'contactus1';
  
 public $fillable = ['name','email','message'];
  
